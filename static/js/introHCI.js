6ba0d98bfb7b307c59ca2cf6748d6a62e57a1ebe
Zheng Hao Tang <zht014@ucsd.edu>
2018-01-24 22:22:44 -0800

    Heroku deployment

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Opps, you did it right");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    //$(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       description.fadeOut();
+    }
 }
