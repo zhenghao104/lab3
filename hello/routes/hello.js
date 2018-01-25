b455026fc91e48466cb0d8d95c2bd7fb0e9b97d9
Zheng Hao Tang <zht014@ucsd.edu>
2018-01-24 23:26:17 -0800

    Lab Submission

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,13 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
-  });
+	var nameToShow = req.params.userName;
+	if (nameToShow == undefined) {
+		nameToShow = 'World';
+	}
+
+	console.log('Name is ' + nameToShow);
+  	res.render('index', {
+  		'name': nameToShow,
+  	});
 };
